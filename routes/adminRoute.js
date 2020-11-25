d7abfbb38af62f958f5b18e5eb17f93e4de14dca
john-paul.vistal <johnpaulvistal@gmail.com>
2020-11-25 11:12:38 +0800

    Admin Controller Added

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -2,8 +2,24 @@ const express = require("express");
 const router = express.Router();
 
 const AdminControllers = require("../controllers/adminController");
+const RegistrationControllers = require("../controllers/adminRegistrationController");
+
 
 router.get("/", AdminControllers.getMainPage);
+
+
+// START ADMINISTRATION ROUTES
+router.post("/registration/", RegistrationControllers.addAdmins);
+
+router.get("/administration/", RegistrationControllers.getAllAdmins);
+
+router.get("/administration/:id", RegistrationControllers.getAdminById);
+
+router.put("/administration/:id", RegistrationControllers.modifyAdmin);
+
+router.delete("/delete/:id", RegistrationControllers.deleteAdmin);
+
+//END ADMINISTRATION ROUTES
 
 
 
--- services/database.js
+++ services/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToDatabase = () => {
     mongoose
-        .connect("mongodb://Politicians:lastGroup@cluster0-shard-00-00.dnbsx.mongodb.net:27017,cluster0-shard-00-01.dnbsx.mongodb.net:27017,cluster0-shard-00-02.dnbsx.mongodb.net:27017/election?ssl=true&replicaSet=atlas-jjnz94-shard-0&authSource=admin&retryWrites=true&w=majority/election_web_app", {
+        .connect("mongodb://electionSystem:election@cluster0-shard-00-00.wr2lp.mongodb.net:27017,cluster0-shard-00-01.wr2lp.mongodb.net:27017,cluster0-shard-00-02.wr2lp.mongodb.net:27017/election_web_app?ssl=true&replicaSet=atlas-atm422-shard-0&authSource=admin&retryWrites=true&w=majority", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
